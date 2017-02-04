365feda9698069ac74c722a73938cb80d78dc0e2
Tom Demets <tom_demets@outlook.com>
2017-02-04 17:34:46 +0100

    20170204

---
+++ Tdmts.ESP8266.BL/TCPClient.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using Windows.Networking;
+using Windows.Networking.Sockets;
+using Windows.Storage.Streams;
+
+namespace Tdmts.ESP8266.BL
+{
+
+    public class TCPClient
+    {
+        private static TCPClient _instance;
+        private StreamSocket _socket;
+
+        public event EventHandler<string> OnConnect;
+        public event EventHandler<string> OnSent;
+        public event EventHandler<string> OnReceived;
+        public event EventHandler<string> OnDisconnect;
+        public event EventHandler<Exception> OnException;
+
+        public static TCPClient getInstance()
+        {
+            if (_instance == null)
+            {
+                _instance = new TCPClient();
+            }
+            return _instance;
+        }
+
+        private TCPClient()
+        {
+            _socket = new StreamSocket();
+        }
+
+        public async void Connect(string host, string port)
+        {
+            try
+            {
+                HostName hostname = new HostName(host);
+                await _socket.ConnectAsync(hostname, port, SocketProtectionLevel.PlainSocket);
+                OnConnect(this, String.Format("Connected to host: {0} on port: {1}", host, port));
+                Receive();
+            }
+            catch (Exception x)
+            {
+                OnException(this, x);
+            }
+        }
+
+        public void Disconnect()
+        {
+            try
+            {
+                _socket.Dispose();
+                OnDisconnect(this, "Disconnected from host");
+            }
+            catch (Exception x)
+            {
+                OnException(this, x);
+            }
+        }
+
+        public async void Send(string message)
+        {
+            try
+            {
+                DataWriter _writer = new DataWriter(_socket.OutputStream);
+                _writer.WriteString(message);
+                await _writer.StoreAsync();
+                _writer.DetachStream();
+                OnSent(this, message);
+            }
+            catch (Exception x)
+            {
+                OnException(this, x);
+            }
+        }
+
+        public async void Receive()
+        {
+            try
+            {
+                DataReader _reader = new DataReader(_socket.InputStream);
+                uint header = await _reader.LoadAsync(4);
+
+                if (header == 0)
+                {
+                    return;
+                }
+
+                int len = _reader.ReadInt32();
+                uint lenBytes = await _reader.LoadAsync((uint)len);
+
+                string message = _reader.ReadString(lenBytes);
+
+                OnReceived(_socket, message);
+
+                Receive();
+            }
+            catch (Exception x)
+            {
+                OnException(this, x);
+            }
+        }
+    }
+}
---
+++ Tdmts.ESP8266.GUI/MainPage.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using Tdmts.ESP8266.BL;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace Tdmts.ESP8266.GUI
+{
+    public sealed partial class MainPage : Page
+    {
+
+        private TCPClient _client = TCPClient.getInstance();
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+            _client.OnConnect += _client_OnConnect;
+            _client.OnDisconnect += _client_OnDisconnect;
+            _client.OnReceived += _client_OnReceived;
+            _client.OnSent += _client_OnSent;
+            _client.OnException += _client_OnException;
+        }
+
+        private void _client_OnConnect(object sender, string e)
+        {
+            txtMessage.Text += String.Format("{0}{1}", e, Environment.NewLine);
+        }
+
+        private void _client_OnDisconnect(object sender, string e)
+        {
+            txtMessage.Text += String.Format("{0}{1}", e, Environment.NewLine);
+        }
+
+        private void _client_OnReceived(object sender, string e)
+        {
+            txtMessage.Text += String.Format("{0}{1}", e, Environment.NewLine);
+        }
+
+        private void _client_OnSent(object sender, string e)
+        {
+            txtMessage.Text += String.Format("{0}{1}", e, Environment.NewLine);
+        }
+
+        private void _client_OnException(object sender, Exception e)
+        {
+            txtMessage.Text += String.Format("{0}{1}", e.Message, Environment.NewLine);
+        }
+
+        private void btnConnect_Click(object sender, RoutedEventArgs e)
+        {
+            _client.Connect(txtIP.Text, txtPort.Text);
+        }
+
+        private void Page_Unloaded(object sender, RoutedEventArgs e)
+        {
+            _client.Disconnect();
+        }
+
+        private void btnDisconnect_Click(object sender, RoutedEventArgs e)
+        {
+            _client.Disconnect();
+        }
+
+        private void btnLedOn_Click(object sender, RoutedEventArgs e)
+        {
+            _client.Send("LED ON");
+        }
+
+        private void btnLedOff_Click(object sender, RoutedEventArgs e)
+        {
+            _client.Send("LED OFF");
+        }
+    }
+}
